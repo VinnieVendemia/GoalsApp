b65bb5ccbd155e4a4a6d106249be96289acdb757
Vinnie Vendemia <vvendemia@jibe.com>
2017-03-29 21:37:28 -0400

    Initial Commit

---
+++ GoalsApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoalsApp
+//
+//  Created by Vinnie Vendemia on 3/29/17.
+//  Copyright (c) 2017 Vinnie Vendemia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ GoalsAppTests/GoalsAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  GoalsAppTests.swift
+//  GoalsAppTests
+//
+//  Created by Vinnie Vendemia on 3/29/17.
+//  Copyright (c) 2017 Vinnie Vendemia. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class GoalsAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
