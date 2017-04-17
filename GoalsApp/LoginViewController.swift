0cc04bcc7461384b5f35b619d9b7f0357bf806e6
vvendemia <vvendemia1@gmail.com>
2017-04-17 18:45:33 -0400

    Add LoginViewController

---
+++ GoalsApp/LoginViewController.swift
@@ -0,0 +1,9 @@
+//
+//  LoginViewController.swift
+//  GoalsApp
+//
+//  Created by Christopher Vincent Vendemia on 4/17/17.
+//  Copyright © 2017 Vinnie Vendemia. All rights reserved.
+//
+
+import Foundation
