9fab535f120bbe1d7786cd270cb11f552a6abd67
vvendemia <vvendemia1@gmail.com>
2017-04-18 19:19:43 -0400

    Add SignUpViewController

---
+++ GoalsApp/SignUpViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SignUpViewController.swift
+//  GoalsApp
+//
+//  Created by Christopher Vincent Vendemia on 4/17/17.
+//  Copyright © 2017 Vinnie Vendemia. All rights reserved.
+//
+
+import Foundation
