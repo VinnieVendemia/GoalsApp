5bedffe59e0f83dcec8b2690e4ed373091fa64ba
vvendemia <vvendemia1@gmail.com>
2017-04-20 17:47:12 -0400

    Add logic to compare password fields on signup page

--- GoalsApp/SignUpViewController.swift
+++ GoalsApp/SignUpViewController.swift
@@ -14,10 +14,32 @@ class SignUpViewController: UIViewController {
     @IBOutlet var emailTextField: UITextField!
     @IBOutlet var passwordVerifyTextField: UITextField!
     @IBOutlet var passwordTextField: UITextField!
+    @IBOutlet var passwordValidationText: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        passwordTextField.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
+        
+        passwordVerifyTextField.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
+    }
+    
+    func textFieldDidChange() {
+        comparePasswordFields();
+        
+    }
+    
+    func comparePasswordFields() {
+        let pw: String = passwordTextField.text!;
+        let pwVerify: String = passwordVerifyTextField.text!;
+        
+        if pw == pwVerify {
+            passwordValidationText.textColor = UIColor.clear;
+        } else {
+            passwordValidationText.textColor = UIColor.white;
+        }
     }
     
 }
