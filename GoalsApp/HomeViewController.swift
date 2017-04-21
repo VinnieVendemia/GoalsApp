1ad6678e8f8c158bdc667736f52eb579b5b8edfa
vvendemia <vvendemia1@gmail.com>
2017-04-21 16:18:33 -0400

    Update code in HomeViewController

--- GoalsApp/HomeViewController.swift
+++ GoalsApp/HomeViewController.swift
@@ -6,4 +6,11 @@
 //  Copyright © 2017 Vinnie Vendemia. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
