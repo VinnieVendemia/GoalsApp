db50db82421fef545ab28b8b3691763513b095c3
vvendemia <vvendemia1@gmail.com>
2017-04-22 16:31:26 -0400

    Add class Goal

---
+++ GoalsApp/Goal.swift
@@ -0,0 +1,17 @@
+//
+//  Goal.swift
+//  GoalsApp
+//
+//  Created by Christopher Vincent Vendemia on 4/22/17.
+//  Copyright © 2017 Vinnie Vendemia. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class GoalTableViewCell: UITableViewCell {
+    
+    //MARK: Properties
+    @IBOutlet var titleLable: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+}
