2d18cbb57a4346b8c883e6ca26fe154969638c7a
vvendemia <vvendemia1@gmail.com>
2017-05-06 15:40:57 -0400

    Add class Progress

---
+++ GoalsApp/Progress.swift
@@ -0,0 +1,28 @@
+//
+//  Progress.swift
+//  GoalsApp
+//
+//  Created by Christopher Vincent Vendemia on 5/6/17.
+//  Copyright © 2017 Vinnie Vendemia. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Progress: Object {
+    
+    dynamic var progressDesciption: String = ""
+    dynamic var date: Date = Date.init()
+    
+    func save() {
+        do {
+            let realm = try Realm()
+            try realm.write {
+                realm.add(self)
+            }
+        } catch let error as NSError {
+            fatalError(error.localizedDescription)
+        }
+    }
+
+}
