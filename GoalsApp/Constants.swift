5c573d4a61606de9c9abe6b5cd516450436c77b3
vvendemia <vvendemia1@gmail.com>
2017-05-18 21:03:41 -0400

    Add class Constants

---
+++ GoalsApp/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  GoalsApp
+//
+//  Created by Christopher Vincent Vendemia on 5/18/17.
+//  Copyright © 2017 Vinnie Vendemia. All rights reserved.
+//
+
+class Constants {
+    
+    static let api_url: String = "https://url-to-my-api.com"
+    
+}
