3b803dc15a799d141dbe9eddc6bfe75dbfab7c93
vvendemia <vvendemia1@gmail.com>
2017-05-27 11:30:56 -0400

    Use goalApiService in ApiWrapper

--- GoalsApp/ApiWrapper.swift
+++ GoalsApp/ApiWrapper.swift
@@ -5,20 +5,23 @@
 //  Created by Christopher Vincent Vendemia on 5/27/17.
 //  Copyright © 2017 Vinnie Vendemia. All rights reserved.
 //
+// This class is intended to contain logic needed to prepare
+// requests and subsequently send data to the API.
 
 import Siesta
 
 class ApiWrapper {
     
-    let goalApi = GoalAPI()
+    let goalApiService = GoalAPIService()
     
     func postGoal(title: String, description: String) {
         var jsonData = [String: String]()
         jsonData["title"] = title
         jsonData["description"] = description
-        goalApi.goals.request(.post, json: jsonData)
+        goalApiService.goals.request(.post, json: jsonData)
             .onSuccess { _ in print("Wow! Data!") }
             .onFailure { error in print("Oh, bummer.") }
     }
-    
 }
+
+let apiWrapper = ApiWrapper()
