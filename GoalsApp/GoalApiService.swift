a1a7864b0392ea25d00f1ca2223ae8dcca0c869d
vvendemia <vvendemia1@gmail.com>
2017-05-27 11:30:28 -0400

    Add todo to GoalApiService

--- GoalsApp/GoalApiService.swift
+++ GoalsApp/GoalApiService.swift
@@ -12,8 +12,10 @@ class GoalAPIService: Service {
     init() {
         super.init(baseURL: Constants.api_url)
         
+        
         configure {
-            $0.headers["token"] = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE0OTU4OTgwNTMsImV4cCI6MTQ5NTk4NDQ1M30.dt3iX3juhi7fFvLdB6xuGoHtWj6hxb6euKtpnvLDdx0"
+            // TODO: Configure based on user-authenticated token
+            $0.headers["token"] = ""
         }
     }
     
