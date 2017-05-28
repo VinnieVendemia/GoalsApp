497864a0bced1780a97128a6a6d50018c6133f2f
vvendemia <vvendemia1@gmail.com>
2017-05-28 14:52:07 -0400

    Add logic to postUser and present alerts on Failure

--- GoalsApp/SignUpViewController.swift
+++ GoalsApp/SignUpViewController.swift
@@ -8,13 +8,15 @@
 
 import UIKit
 
-class SignUpViewController: UIViewController {
+class SignUpViewController: UIViewController, UITextFieldDelegate {
     
     
     @IBOutlet var emailTextField: UITextField!
     @IBOutlet var passwordVerifyTextField: UITextField!
     @IBOutlet var passwordTextField: UITextField!
     @IBOutlet var passwordValidationText: UILabel!
+    var passwordsMatch = false
+    @IBOutlet weak var saveButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +30,7 @@ class SignUpViewController: UIViewController {
         passwordTextField.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
         
         passwordVerifyTextField.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
+        
     }
     
     func textFieldDidChange() {
@@ -40,10 +43,39 @@ class SignUpViewController: UIViewController {
         let pwVerify: String = passwordVerifyTextField.text!;
         
         if pw == pwVerify {
+            passwordsMatch = true
             passwordValidationText.textColor = UIColor.clear;
         } else {
+            passwordsMatch = false
             passwordValidationText.textColor = UIColor.white;
         }
     }
+
     
+    //MARK: Actions
+    @IBAction func goToGoalsController(_ sender: Any) {
+
+        if(!passwordsMatch) {
+            let alert = UIAlertController(title: "Error", message: "Passwords do not match", preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        } else {
+        
+            let username = emailTextField.text
+            let password = passwordTextField.text
+            
+            apiWrapper.postUser(username: username!, password: password!, errorAlert: failureAlert() )
+            // If positive response, call AUTH user, save token locally
+                // then proceed to goalsController
+            // if negative response, alert user that the save failed
+        
+            // self.performSegue(withIdentifier: "GoToGoalsController", sender:self)
+        }
+    }
+    
+    func failureAlert() {
+        let alert = UIAlertController(title: "Error", message: "Failed to create user", preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
 }
